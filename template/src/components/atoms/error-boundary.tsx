f23dfe0adb20f37d1a6d6a55f18c35c49fb3fe23
dodicandra <dodicandra20@gmail.com>
2023-01-25 09:11:21 +0700

    upgrade rn 71

--- template/src/components/atoms/error-boundary.tsx
+++ template/src/components/atoms/error-boundary.tsx
@@ -22,6 +22,7 @@ type State = {hasError: Error | null};
 
 type Props = {
   onError?(error: Error, stack: string): void;
+  children?: React.ReactElement;
 };
 
 class ErrorBoundary extends React.PureComponent<Props, State> {
@@ -49,11 +50,7 @@ class ErrorBoundary extends React.PureComponent<Props, State> {
 
   render() {
     const state = this.state;
-    return this.state.hasError ? (
-      <FallBack error={state.hasError} onPress={this.resetError} />
-    ) : (
-      this.props.children
-    );
+    return this.state.hasError ? <FallBack error={state.hasError} onPress={this.resetError} /> : this.props.children;
   }
 }
 
--- template/src/components/organisms/provider.tsx
+++ template/src/components/organisms/provider.tsx
@@ -7,7 +7,7 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {QueryProvider} from '@react-query/query-provider';
 import {persist, store} from '@redux-store/store';
 
-const AppProvider: React.FC = ({children}) => {
+const AppProvider: React.FC<{children?: React.ReactElement}> = ({children}) => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persist}>
