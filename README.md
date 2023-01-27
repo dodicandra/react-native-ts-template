88d62129fdfa9c66d206d3c2afefd906e50fb91d
dodicandra <dodicandra20@gmail.com>
2023-01-27 14:10:50 +0700

    feat: RN 69

--- README.md
+++ README.md
@@ -20,7 +20,8 @@ See the below table to find out which version of the template to use.
 
 | React Native | Template | Packages                                             |
 | ------------ | -------- | ---------------------------------------------------- |
-| 0.71         | 0.4.\*   | react query, MMKV, redux, i18n, ky                   |
-| 0.70         | 0.3.\*   | react query, MMKV, redux, i18n, ky                   |
-| 0.67         | 0.2.\*   | react query, MMKV, redux, i18n, ky, react navigation |
+| 0.71         | 0.4.\*   | react query, MMKV, redux, i18n, ky, react navigation |
+| 0.70         | 0.3.\*   | react query, MMKV, redux, i18n, ky, react navigation |
+| 0.69         | 0.2.3    | react query, MMKV, redux, i18n, ky, react navigation |
+| 0.67         | 0.2.2    | react query, MMKV, redux, i18n, ky, react navigation |
 | 0.67         | 0.1.\*   | react query, MMKV, redux, i18n, ky                   |
--- template/android/app/src/main/java/com/awesome/MainApplication.java
+++ template/android/app/src/main/java/com/awesome/MainApplication.java
@@ -1,19 +1,22 @@
 package com.awesome;
 
 import android.app.Application;
+import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.awesome.newarchitecture.MainApplicationReactNativeHost;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
+      new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -31,16 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
-
-        @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
         }
       };
 
@@ -62,10 +55,37 @@ public class MainApplication extends Application implements ReactApplication {
     // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   *
+   * @param context
+   * @param reactInstanceManager
+   */
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.awesome.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
